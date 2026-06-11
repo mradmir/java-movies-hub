5b77d9ba2ef330f54048f338b2538a10a98e21bc
Radmir Molotov <radmirmolotov@gmail.com>
2026-06-11 22:29:12 +0500

    Удалил ненужные импорты

--- src/main/ru/practicum/moviehub/http/MoviesServer.java
+++ src/main/ru/practicum/moviehub/http/MoviesServer.java
@@ -1,19 +1,9 @@
 package ru.practicum.moviehub.http;
 
 import com.sun.net.httpserver.HttpServer;
-import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
-import java.time.Duration;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MoviesServer {
     private final HttpServer server;
